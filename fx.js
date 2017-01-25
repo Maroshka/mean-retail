fb24aa2c525b898e847c01530fd39fa23b989570
maroshka <maroshka202@gmail.com>
2017-01-25 22:07:11 +0200

    25-jan-2017

---
+++ fx.js
@@ -0,0 +1,10 @@
+/**
+ * Created by muna on 25/01/17.
+ */
+
+modules.exports = function () {
+  return {
+      'USD':1,
+      'JD':0.71
+  }
+};
--- product.js
+++ product.js
@@ -3,6 +3,7 @@
  */
 var mongoose = require('mongoose');
 var Category = require('./category');
+var fx       = require('./fx');
 
 var productSchema = {
     name: {
@@ -18,17 +19,40 @@ var productSchema = {
     price: {
         amount:{
             type: Number,
-            required: true
+            required: true,
+            set: function(v){
+                this.internal.approximatePriceUSD =
+                    v / (fx()[this.price.currency] || 1);
+                return v;
+            }
         },
         currency:{
             type: String,
             enum: ['USD', 'JD'],
-            required: true
+            required: true,
+            set: function(v){
+                this.internal.approximatePriceUSD =
+                    this.price.amount/ (fx()[v] || 1);
+                return v;
+            }
         },
-        category: Category.categorySchema
+        category: Category.categorySchema,
+        internal: {
+            approximatePriceUSD: Number
+        }
 
     }
 };
+
+var schema = new mongoose.Schema(productSchema);
+var currencySymbols = {
+    'USD': '$',
+    'JD': 'JD'
+};
+
+schema.virtual('displayPrice').get(function(){
+    return currencySymbols[this.price.currency]+' '+this.price.amount;
+});
 
 module.exports = new mongoose.Schema(productSchema);
 module.exports.productSchema = productSchema;
