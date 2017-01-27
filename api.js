ee0ce148622d154c73e6b41ac70a1cedd2a758b5
maroshka <maroshka202@gmail.com>
2017-01-27 14:52:04 +0200

    27-jan-2017

--- api.js
+++ api.js
@@ -9,58 +9,38 @@ module.exports = function(wagner){
   });
   api.get('/category/id/:id', wagner.invoke(function(Category){
     return function(req, res){
-      Category.findOne({ _id : req.params.id}, function(error, category){
-        if(error){
-          return res.status(status.INTERNAL_SERVER_ERROR).json({error:error.toString()});
-        }
-        if(!category){
-          return res.status(status.NOT_FOUND).json({error:'Not Found!'});
-        }
-        res.json({category:category});
-      });
+      Category.findOne({ _id : req.params.id}, handleRes.bind(null,'category', res));
     };
   }));
   api.get('/category/parent/:id', wagner.invoke(function(Category){
     return function(req, res){
-      Category.find({ parent:req.params.id}).sort({_id:1}).exec(function(error, categories){
-        if(error){
-          return res.status(status.INTERNAL_SERVER_ERROR).json({error:error.toString()});
-        }
-        if(!categories){
-          return res.status(status.NOT_FOUND).json({error:'Not Found!'});
-        }
-        res.json({categories:categories});
-      });
+      Category.find({ parent:req.params.id}).sort({_id:1}).exec(handleRes.bind(null, 'categories',res));
     };
   }));
 
   api.get('/product/id/:id', wagner.invoke(function(Product){
     return function(req, res){
-      Product.findOne({_id: req.params.id}, function(error, product){
-        if(error){
-          return res.status(status.INTERNAL_SERVER_ERROR).json({error:error.toString()});
-        }
-        if(!product){
-          return res.status(status.NOT_FOUND).json({error:'Product not found!'});
-        }
-        res.json({product:product});
-      });
+      Product.findOne({_id: req.params.id}, handleRes.bind(null, 'product', res));
     };
   }));
 
   api.get('/product/category/:id', wagner.invoke(function(Product){
     return function(req, res){
       var sort = {name:1};
-      Product.find({'category.ancestors':req.params.id}).sort(sort).exec(function(error, products){
-        if(error){
-          return res.status(status.INTERNAL_SERVER_ERROR).json({error:error});
-        }
-        if(!products){
-          return res.status(status.NOT_FOUND).json({error:'Products of the terget category were not found!'});
-        }
-        res.json({products:products});
-      });
+      Product.find({'category.ancestors':req.params.id}).sort(sort).exec(handleRes.bind(null, 'products', res));
     };
   }));
   return api;
 };
+
+function handleRes(key, res, error, result){
+  if(error){
+    return res.status(status.INTERNAL_SERVER_ERROR).json({error:error.toString()});
+  }
+  if(!result){
+    return res.status(status.NOT_FOUND).json({error:key+' not found!'});
+  }
+  var json = {};
+  json[key] = result;
+  res.json(json);
+}
